50e37cb50aa1d92e586d9ae9341c1f8a3854578d
Nhat <nmd@fastmail.fm>
2020-05-25 17:36:32 +0800

    Updates

---
+++ precache-manifest.1aa94fbc5381986fa16cf6e269e8a41a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "785a53977bfb4a07d13314ebaa8dc93f",
+    "url": "/moviedb/index.html"
+  },
+  {
+    "revision": "257074015a0dc7209dfd",
+    "url": "/moviedb/static/css/main.a5065a3d.chunk.css"
+  },
+  {
+    "revision": "56c84ad3ca6b8f568c7f",
+    "url": "/moviedb/static/js/2.e01b5bda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviedb/static/js/2.e01b5bda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "257074015a0dc7209dfd",
+    "url": "/moviedb/static/js/main.2492aa7b.chunk.js"
+  },
+  {
+    "revision": "3ef2c0b5513e19a297e2",
+    "url": "/moviedb/static/js/runtime-main.3878a2e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/moviedb/static/media/logo.5d5d9eef.svg"
+  }
+]);
